f5706d34db31d3781169bf5db39b3760e2d0c044
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-01-09 22:24:44 +0700

    feat:finish all ex

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import assignmentRouter from "./apps/assignments.js";
+import loggingMiddleware from "./logging.js";
 
 const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
+app.use(loggingMiddleware);
 app.use("/assignments", assignmentRouter);
 
 app.listen(port, () => {
